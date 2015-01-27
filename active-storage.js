5d38ba73a731bb66dad74719c2ec1044a9177db3
Jesse Shawl <jesse@jshawl.com>
2015-01-27 09:18:58 -0500

    add destroy method

--- active-storage.js
+++ active-storage.js
@@ -10,20 +10,33 @@ ActiveStorage.prototype = {
   },
   save: function(){
     var self = Object.create( this );
+    var id = this.all.length + 1;
     for( prop in self.data ){
       self[prop] = self.data[prop]; 
     }
+    self.id = id;
     this.all.push( self );	
     return self;
   },
   create: function( data ){
-    this.data = data;
-    this.save();
+    var self = Object.create( this );
+    self.data = data;
+    self.save();
+  },
+  find: function( id ){
+    return _.where( this.all, {id: id} )[0];
   },
   find_by: function( predicate ){
     return _.find( this.all, predicate ); 
   },
   where: function( predicate ){
     return _.where( this.all, predicate );
+  },
+  destroy: function(){
+    var self = this.data;
+    this.all = _.filter( this.all, function( o ){
+      return o.data != self;
+    });
+    ActiveStorage.prototype.all = this.all;
   }
 }
