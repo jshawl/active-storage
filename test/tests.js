bee2e24f64f791a1cfd62fba79523c26c0dbb5ad
Jesse Shawl <jesse@jshawl.com>
2015-01-28 17:13:17 -0500

    refactor tests

--- test/tests.js
+++ test/tests.js
@@ -1,27 +1,28 @@
 (function(){
+  module("Post", {
+    beforeEach: function(){
+      this.Post = Object.create( new ActiveStorage("Post") );
+      this.Post.destroyAll();
+    }
+  })
   test('can be created', function( assert ){
-    var Post = Object.create( new ActiveStorage("Post") );
-    assert.notEqual( Post, undefined);
+    assert.notEqual( this.Post, undefined);
   });
   test('has class methods', function( assert ){
-    var Post = Object.create( new ActiveStorage("Post") );
-    assert.notEqual( Post.new, undefined);
-    assert.notEqual( Post.create, undefined);
-    assert.notEqual( Post.find, undefined);
+    assert.notEqual( this.Post.new, undefined);
+    assert.notEqual( this.Post.create, undefined);
+    assert.notEqual( this.Post.find, undefined);
   });
   test('has instance methods', function( assert ){
-    var Post = Object.create( new ActiveStorage("Post") );
-    var p = Post.new();
+    var p = this.Post.new();
     assert.notEqual( p.save, undefined);
     assert.notEqual( p.destroy, undefined);
   });
   test('find', function( assert ){
-    var Post = Object.create( new ActiveStorage("Post") );
-    Post.destroyAll();
-    Post.create({name: "Jesse"});
-    var p = Post.find( 1 );
-    assert.equal( p, Post );
-    p = Post.find( 100 );
+    this.Post.create({name: "Jesse"});
+    var p = this.Post.find( 1 );
+    assert.equal( p, this.Post );
+    p = this.Post.find( 100 );
     assert.equal( p, undefined );
   });
 })()
