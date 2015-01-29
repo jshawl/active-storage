7e61007f498ef9f7f985475c2a97bffa557bf028
Jesse Shawl <jesse@jshawl.com>
2015-01-28 20:40:40 -0500

    namespace in iife

--- active-storage.js
+++ active-storage.js
@@ -1,67 +1,79 @@
-var ActiveStorage = function( kind ){ 
-  this.kind = kind;
-}
+var ActiveStorage = (function(){
 
-ActiveStorage.prototype = {
-  all: function(){
-    if( localStorage.getItem( this.kind ) ){
-      return JSON.parse( localStorage.getItem( this.kind ) ); 
-    } else {
-      return [];
-    }     
-  },
-  new: function( data ){
-    for( var prop in data ){
-      this[prop] = data[prop]; 
-    }
-    return this;     
-  },
-  save: function(){
-    var all = this.all();
-    if( !this.id ){
-      this.id = all.length + 1;
-      all.push( this );
-    } else {
-      all[this.id - 1] = this;
-    }
-    localStorage.setItem(this.kind, JSON.stringify( all ));
-    return this;
-  },
-  create: function( data ){
-    this.new( data );
-    this.id = false;
-    this.save();
-    return this;
-  },
-  find: function( id ){
-    var obj = _.where( this.all(), {id: id} )[0];
-    for( var prop in obj ){
-      this[prop] = obj[prop];
-    }
-    if( obj ){
-      return this;
-    } else {
-      return undefined;
-    }
-  },
-  findBy: function( predicate ){
-    var obj = _.find( this.all(), predicate ); 
-    for( var prop in obj ){
-      this[prop] = obj[prop];
-    }
-    return this;
-  },
-  where: function( predicate ){
-    return _.where( this.all(), predicate );
-  },
-  destroy: function(){
-    var self = this;
-    var all = _.filter( self.all(), function( o ){
-      return o.id != self.id; 
-    });
-    localStorage.setItem( self.kind, JSON.stringify( all ) );
-  },
-  destroyAll: function(){
-    localStorage.setItem( this.kind, "[]" );
+  var ActiveStorage = function( kind ){ 
+    this.kind = kind;
   }
-};
+
+  function propify( self, data ){
+    for( var prop in data ){
+      self[prop] = data[prop]; 
+    }
+    return self;     
+  }
+
+  ActiveStorage.prototype = {
+    all: function(){
+      if( localStorage.getItem( this.kind ) ){
+	return JSON.parse( localStorage.getItem( this.kind ) ); 
+      } else {
+	return [];
+      }     
+    },
+    new: function( data ){
+      return propify( this, data );
+    },
+    save: function(){
+      var all = this.all();
+      if( !this.id ){
+	this.id = all.length + 1;
+	all.push( this );
+      } else {
+	all[this.id - 1] = this;
+      }
+      localStorage.setItem(this.kind, JSON.stringify( all ));
+      return this;
+    },
+    create: function( data ){
+      this.new( data );
+      this.id = false;
+      return this.save();
+    },
+    update: function( data ){
+      return propify( this, data ).save();
+    },
+    find: function( id ){
+      var obj = _.where( this.all(), {id: id} )[0];
+      if( obj ){
+	return propify( this, obj );
+      } else {
+	return undefined;
+      }
+    },
+    findBy: function( predicate ){
+      var obj = _.find( this.all(), predicate ); 
+      if( obj ){
+	return propify( this, obj );
+      } else {
+	return undefined;
+      }
+    },
+    where: function( predicate ){
+      var all = _.where( this.all(), predicate );
+      var self = this;
+      return _.map( all, function( o ){
+        return propify( self, o ); 
+      });
+    },
+    destroy: function(){
+      var self = this;
+      var all = _.filter( self.all(), function( o ){
+	return o.id != self.id; 
+      });
+      localStorage.setItem( self.kind, JSON.stringify( all ) );
+    },
+    destroyAll: function(){
+      localStorage.setItem( this.kind, "[]" );
+    }
+  };
+  return ActiveStorage;
+})();
--- test/tests.js
+++ test/tests.js
@@ -25,15 +25,25 @@
     p = this.Post.find( 100 );
     assert.equal( p, undefined );
   });
+  test('findBy', function( assert ){
+    this.Post.create({name: "Jesse"});
+
+    var p = this.Post.findBy( {name: "Jesse"} );
+    assert.equal( p, this.Post, "it can be found");
+
+    p = this.Post.findBy({name:"Meow"});
+    assert.equal( p, undefined, "it can be found if it exists");
+  });
   test('create', function( assert ){
     var p = this.Post.create({name: "Jesse"});
-    assert.equal( p, this.Post );
+    assert.equal( p, this.Post, "it should be a post");
+    assert.equal( p.id, 1, "it should have an id");
   });
   test('destroy', function( assert ){
     var p = this.Post.create({name: "Jesse"});
-    p.destroy();
     var len = this.Post.all().length;
-    assert.equal( len, 0 );
+    p.destroy();
+    assert.equal( len, 1 );
   });
   test('all', function( assert ){
     this.Post.create({name: "Jesse"});
@@ -46,5 +56,28 @@
     this.Post.destroyAll();
     var len = this.Post.all().length;
     assert.equal( len, 0 );
+  });
+  test('save', function( assert ){
+    var p = this.Post.new({name: "Jesse"});
+    assert.equal( p.id, undefined );
+
+    p.save();
+
+    assert.equal( p.id, 1, "it should have an id");
+    assert.equal( this.Post.all().length, 1, "it should be in the collection");
+  });
+  test('where', function( assert ){
+    this.Post.create({name: "Jesse"});
+    var where = this.Post.where({name: "Jesse"});
+    assert.equal( where.constructor, Array, "it should return an array");
+    assert.equal( where[0], this.Post, "it should return an array of posts");
+  });
+  test('update', function( assert ){
+    var p = this.Post.create({name: "Jesse"});
+    p.update({name: "Adam"});
+    assert.equal( p.name, "Adam", "it should update existing attributes");
+    p.update({age: 23});
+    assert.equal( p.name, "Adam", "it should not remove existing attributes");
+    assert.equal( this.Post.all()[0].name, "Adam", "it should save to localstorage");
   });
 })()
